d0a902bf6f6e75007a564549ed48e5e85b073291
Adedotun Sobambo <sobamboadedotun@gmail.com>
2021-05-18 16:43:43 +0100

    updated

--- src/components/reusable/Input.js
+++ src/components/reusable/Input.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Input = () => {
+    const [text, setText] = useState('')
     return (
         <div className= "form-control">
             <label htmlFor="text">Text</label>
-            <input type="text" placeholder="Enter text..."/>
-            
+            <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..."/>
+            <h5>{text}</h5>
         </div>
     )
 }
